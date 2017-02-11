6b2e1393061da19a43df49d295e2923923c2ad33
Paweł Okoń <pawel@okon.eu>
2017-02-11 10:28:05 +0100

    Update test_result to match log fie without issues

--- parser.rb
+++ parser.rb
@@ -4,8 +4,8 @@ def collect_test_times(file_path)
   results = []
   if File.exists?(file_path)
     File.open(file_path).each do |line|
-      test_result = line.match(/(([0-9]{2})\.([0-9]{1,2}))/)[0]
-      results.push(test_result.to_f)
+      test_result = line.match(/(([0-9]{2})\.([0-9]{1,2}))/)
+      results.push(test_result[0].to_f) if test_result
     end
   end
 
